8589a777982fd5f8ff01971f96f4f395b4a7e725
Jake14turner <110646968+Jake14turner@users.noreply.github.com>
2024-10-01 19:45:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import streamlit as st
+import sqlite3 as sq
 
-# Title of the app
+
+
+
 st.title("3444 project")
+
+
+
+registerPage = st.Page("registerPage.py", title="Register Page", icon=":material/add_circle:")
+loginPage = st.Page("loginPage.py", title="Login Page", icon=":material/delete:")
+homePage = st.Page("homePage.py", title="Home Page", icon=":material/add_circle:")
+
+
+
+
+pg = st.navigation([registerPage, loginPage, homePage])
+
+pg.run()
+
+
+
+
+    
+
