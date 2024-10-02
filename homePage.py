77788085708a87b493e0f6fc3558a4a8211c3827
Jake14turner <110646968+Jake14turner@users.noreply.github.com>
2024-10-01 21:03:32 -0500

    Update homePage.py

--- homePage.py
+++ homePage.py
@@ -28,59 +28,45 @@ def homePageView():
 
         
         #if the token has already been saved 
-        if st.session_state.token_saved:
-            st.success("Token saved.")
+
+        #if st.session_state.token_saved:
+        #    st.success("Token saved.")
+        #    
+#
+##            testToken = connect.fetchone()
+ #           #The users token is stored in userToken
+ #           userToken = testToken[0]
             
 
-            connect.execute('''SELECT key FROM users WHERE username = ?''', (username,))
-            testToken = connect.fetchone()
-            #The users token is stored in userToken
-            userToken = testToken[0]
-            
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        else:
+     #   else:
             #the first thing we need to do is make the user enter their key and then write that to their name in the database
-            st.text("Lets start off by connecting your canvas account. Please input your canvas token:")
+        st.text("Lets start off by connecting your canvas account. Please input your canvas token:")
             #put the users token into token
-            token = ""
-            token = st.text_input("Please enter your token")
-            testToken = ""
+        token = ""
+        token = st.text_input("Please enter your token")
+        testToken = ""
 
             
-            if st.button("Submit token"):
+        if st.button("Submit token"):
 
-                if token and username:
-                        testToken = ""
+            if token and username:
+                    testToken = ""
 
-                        #connect to the sqlite database and put the users token in
+                    #connect to the sqlite database and put the users token in
                         
-                        connect.execute('''UPDATE users SET key = ? WHERE username = ? ''', (token, username))
-                        connection.commit()
-                        connect.execute('''SELECT key FROM users WHERE username = ?''', (username,))
-                        testToken = connect.fetchone()
-                        if testToken and testToken[0] == token:
-                            st.text("Click again to go home")
-                            st.success(f"Token saved for {username}.")
+                    connect.execute('''UPDATE users SET key = ? WHERE username = ? ''', (token, username))
+                    connection.commit()
+                    connect.execute('''SELECT key FROM users WHERE username = ?''', (username,))
+                    testToken = connect.fetchone()
+                    if testToken and testToken[0] == token:
+                        st.text("Click again to go home")
+                        st.success(f"Token saved for {username}.")
                             
-                            st.session_state.bool1 = True
-                            st.session_state.token_saved = True
-                        else:
-                            st.error(f"Failed to save or retrieve the token for user {username}.")
+                        st.session_state.bool1 = True
+                        st.session_state.token_saved = True
+                    else:
+                        st.error(f"Failed to save or retrieve the token for user {username}.")
     else:
         #create a list for course id's as well as names
         courseIDList = []
