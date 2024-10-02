ba6d60d3455b79245411e2e3f04943cbcac3d09b
Jake14turner <110646968+Jake14turner@users.noreply.github.com>
2024-10-01 20:37:23 -0500

    Update homePage.py

--- homePage.py
+++ homePage.py
@@ -3,33 +3,72 @@ import sqlite3 as sqlite3
 import hashlib
 from loginPage import isLoggedIn
 
+st.session_state.bool1 = False
 
 
+if 'token_saved' not in st.session_state:
+    st.session_state.token_saved = False
+
+
+connection = sqlite3.connect('streamlitBase')
+connect = connection.cursor()
 
 def homePageView():
-    
-    #the first thing we need to do is make the user enter their key and then write that to their name in the database
-    st.text("Lets start off by connecting your canvas account. Please input your canvas token:")
-    #put the users token into token
-    token = ""
-    token = st.text_input("Please enter your token")
-    testToken = ""
-    
-    if st.button("Submit token"):
+    username = st.session_state.username
+    #if the token has already been saved 
+    if st.session_state.token_saved:
+        st.success("Token saved.")
+        
 
-       
-        testToken = ""
-        username = st.session_state.username
-        #connect to the sqlite database and put the users token in
-        connection = sqlite3.connect('streamlitBase')
-        connect = connection.cursor()
-        connect.execute('''UPDATE users SET key = ? WHERE username = ? ''', (token, username))
-        connection.commit()
         connect.execute('''SELECT key FROM users WHERE username = ?''', (username,))
         testToken = connect.fetchone()
-    if testToken == token:
-            st.success("Succesfully saved token")
+        #The users token is stored in userToken
+        userToken = testToken[0]
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    else:
+        #the first thing we need to do is make the user enter their key and then write that to their name in the database
+        st.text("Lets start off by connecting your canvas account. Please input your canvas token:")
+        #put the users token into token
+        token = ""
+        token = st.text_input("Please enter your token")
+        testToken = ""
+
+        
+        if st.button("Submit token"):
+
+            if token and username:
+                    testToken = ""
+
+                    #connect to the sqlite database and put the users token in
+                    
+                    connect.execute('''UPDATE users SET key = ? WHERE username = ? ''', (token, username))
+                    connection.commit()
+                    connect.execute('''SELECT key FROM users WHERE username = ?''', (username,))
+                    testToken = connect.fetchone()
+                    if testToken and testToken[0] == token:
+                        st.success(f"Token saved for {username}.")
+                        
+                        st.session_state.bool1 = True
+                        st.session_state.token_saved = True
+                    else:
+                        st.error(f"Failed to save or retrieve the token for user {username}.")
+
 
 
 
@@ -52,7 +91,7 @@ if 'isLoggedIn' not in st.session_state:
 
 
 #Check if the user is logged in, if they are display the homePage
-if st.session_state.isLoggedIn:
+if st.session_state.isLoggedIn and st.session_state.bool1 == False:
     homePageView()
 else:
     st.text("Please log in to view home page.")
