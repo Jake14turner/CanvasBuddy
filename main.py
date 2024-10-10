8d4caa845a59b747a57e701b4cc2c9499a8432f6
Jake14turner <110646968+Jake14turner@users.noreply.github.com>
2024-10-10 11:51:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ st.title("3444 project")
 registerPage = st.Page("registerPage.py", title="Register Page", icon=":material/add_circle:")
 loginPage = st.Page("loginPage.py", title="Login Page", icon=":material/delete:")
 homePage = st.Page("homePage.py", title="Home Page", icon=":material/add_circle:")
+todopage = st.Page("todopage.py", title = "To do page")
 
 
 
